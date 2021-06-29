be9d9dac9b84532054921706659f3ddb364c840b
HarshaSudheer <67200088+HarshaSudheer@users.noreply.github.com>
2021-06-29 13:03:24 +0530

    Create javascript file

---
+++ index.js
@@ -0,0 +1,24 @@
+function function1(){
+	var rating=prompt("Give your rating for Lucifer out of 5");
+	document.getElementById("rating1").innerHTML=rating;
+}
+function function2(){
+	var rating=prompt("Give your rating for The Bold Type out of 5");
+	document.getElementById("rating2").innerHTML=rating;
+}
+function function3(){
+	var rating=prompt("Give your rating for The Stranger Things out of 5");
+	document.getElementById("rating1").innerHTML=rating;
+}
+function function4(){
+	var rating=prompt("Give your rating for The Vampire Diaries out of 5");
+	document.getElementById("rating1").innerHTML=rating;
+}
+function function5(){
+	var rating=prompt("Give your rating for Money Heist out of 5");
+	document.getElementById("rating1").innerHTML=rating;
+}
+function function6(){
+	var rating=prompt("Give your rating for Suits out of 5");
+	document.getElementById("rating1").innerHTML=rating;
+}
